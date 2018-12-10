ecc429d2868aaece2d05f9705c8ef22c0ba6f988
Unknown <norama.matema@gmail.com>
2018-12-10 22:23:33 +0100

    add: router

---
+++ src/Main.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, NavLink } from 'react-router-dom';
+import { Nav, NavItem } from 'reactstrap';
+
+import './Main.css';
+
+const Main = () => (
+	<Router>
+		<div>
+			<Nav>
+				<NavItem>
+					<NavLink to="/contacts" activeClassName="selected">Kontakty</NavLink>
+				</NavItem>
+				<NavItem>
+					<NavLink to="/quiz" activeClassName="selected">Kvíz</NavLink>
+				</NavItem>
+			</Nav>
+
+			<Route path="/" render={() => (<Redirect to="/contacts"/>)} />
+			<Route path="/contacts" component={Contacts} />
+			<Route path="/quiz" component={Quiz} />
+		</div>
+	</Router>
+);
+
+const Contacts = () => (
+	<h2>Contacts</h2>
+);
+
+
+const Quiz = () => (
+	<h2>Quiz</h2>
+);
+
+export default Main;
