6bf46655791962d896ead205aa5bfc1b2da36413
Nora Mate <norama.matema@gmail.com>
2018-12-15 09:34:29 +0100

    Updates

---
+++ precache-manifest.4e2bd926a26bcdd7611027466f58af43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3068285a94e79392a7d0",
+    "url": "/quiz/static/js/runtime~main.3068285a.js"
+  },
+  {
+    "revision": "9e552ab71eed53a41ff1",
+    "url": "/quiz/static/js/main.9e552ab7.chunk.js"
+  },
+  {
+    "revision": "d366705e4a6097e4a42d",
+    "url": "/quiz/static/js/1.d366705e.chunk.js"
+  },
+  {
+    "revision": "9e552ab71eed53a41ff1",
+    "url": "/quiz/static/css/main.18d957a8.chunk.css"
+  },
+  {
+    "revision": "d366705e4a6097e4a42d",
+    "url": "/quiz/static/css/1.87de3901.chunk.css"
+  },
+  {
+    "revision": "a9bc034e6583cc8b304da48df650eb56",
+    "url": "/quiz/index.html"
+  }
+];
