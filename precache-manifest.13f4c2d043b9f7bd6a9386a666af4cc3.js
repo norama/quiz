68a468cdd09d6f3dc75efa778a7f091b7b3c0746
Nóra Máté <norama.matema@gmail.com>
2019-11-21 20:48:31 +0100

    Updates

---
+++ precache-manifest.13f4c2d043b9f7bd6a9386a666af4cc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd217095590de50aa436449ffb4244d6",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "2a914c563dada6961dfd",
+    "url": "/quiz/static/css/2.9720967d.chunk.css"
+  },
+  {
+    "revision": "c76df76c478dcbb9e84e",
+    "url": "/quiz/static/css/main.0d4f748e.chunk.css"
+  },
+  {
+    "revision": "2a914c563dada6961dfd",
+    "url": "/quiz/static/js/2.07b2e0cd.chunk.js"
+  },
+  {
+    "revision": "c76df76c478dcbb9e84e",
+    "url": "/quiz/static/js/main.8891fa3f.chunk.js"
+  },
+  {
+    "revision": "bb15db22ca3fa8827fec",
+    "url": "/quiz/static/js/runtime-main.759e9fed.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quiz/precache-manifest.664d299ffb471b716df368818a4f1b97.js"
+  "/quiz/precache-manifest.13f4c2d043b9f7bd6a9386a666af4cc3.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/quiz/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quiz/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
